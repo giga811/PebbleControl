c2f2e62bd97544b45e93fd8c29855d5f49553803
Orgil <giga811@gmail.com>
2015-07-30 19:35:29 +0900

    PebbleFlask v0.2 added web gui

--- pebbleFlask.py
+++ pebbleFlask.py
@@ -6,7 +6,7 @@
 
 """
 # flask imports
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 # configuration
@@ -21,6 +21,14 @@ def keyCode(keycode):
     cmd = """
     osascript -e 'tell application "System Events"
     key code """ + keycode + """
+    end tell'
+    """
+    os.system(cmd)
+
+def alphabetStroke(c):
+    cmd = """
+    osascript -e 'tell application "System Events"
+    keystroke \"""" + c[0] + """\"
     end tell'
     """
     os.system(cmd)
@@ -50,6 +58,30 @@ def goleft():
     keyCode("123")
     print "Go left"
     return "ok"
+
+@app.route('/keycode/<int:x>')
+def keycode_controller(x):
+    # this keycode_controller has some security risk
+    if x > 128 or x < 0:
+        return "invalid keycode"
+    else:
+        keyCode(str(x))
+        print "Keycode ", x
+        return "ok"
+
+@app.route('/keychar/<string:c>')
+def keychar_controller(c):
+    if len(c) > 1:
+        return "invalid keychar"
+    else:
+        alphabetStroke(c)
+        print "Keychar ", c
+        return "ok"
+
+@app.route('/control.html')
+def controlhtml():
+    return render_template('control.html')
+
 
 @app.route('/')
 def index():
